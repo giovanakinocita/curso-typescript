a8ddb1aa555326adb2f3180e70f134ec46d1448d
Giovana Kinocita <giovanakinocita@hotmail.com>
2023-09-10 19:05:17 -0300

    Aprendizado: limitações do Javascript

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,6 +1,12 @@
 import {Negociacao} from './models/negociacao.js';
 
 const negociacao = new Negociacao(new Date(), 10, 100);
-console.log(negociacao);
-negociacao.quantidade = 1000;
-console.log(negociacao);
+console.log(negociacao.volume);
+
+
+// Nesta aula aprendi:
+//
+// * Introdução ao projeto e sua estrutura;
+// * Um pouco sobre módulos do ECMASCRIPT;
+// * Modelagem de uma Negociação em Javascript;
+// * Buracos em nossa modelagem por limitações da linguagem Javascript.

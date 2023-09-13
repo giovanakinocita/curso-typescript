a239ec0f26f051cd580e0e38d0b8a8f39a54a0d4
Giovana Kinocita <giovanakinocita@hotmail.com>
2023-09-13 16:29:26 -0300

    Manipulação do DOM: querySelector(), preventDefault(), addEventListener()

--- app/app.ts
+++ app/app.ts
@@ -1,10 +1,9 @@
-import {Negociacao} from './models/negociacao.js';
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
 
-const negociacao = new Negociacao(new Date(), 10, 100);
-console.log(negociacao.volume);
+const controller = new NegociacaoController();
+const form = document.querySelector('.form');
 
-
-// Aprendizado da aula:
-//
-// Modificador private para melhor adaptação dos
-// navegadores atuais (09/2023)
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    controller.adiciona();
+});
--- dist/js/app.js
+++ dist/js/app.js
@@ -1,8 +1,3 @@
 import { Negociacao } from './models/negociacao.js';
 const negociacao = new Negociacao(new Date(), 10, 100);
 console.log(negociacao.volume);
-
-// Aprendizado da aula:
-//
-// Modificador private para melhor adaptação dos
-// navegadores atuais (09/2023)

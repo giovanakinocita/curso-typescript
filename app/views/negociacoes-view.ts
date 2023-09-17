5bcc2910c570f1b5d2e91ba4fde99e8eb2810eec
Giovana Kinocita <giovanakinocita@hotmail.com>
2023-09-17 13:47:42 -0300

    Um pouco de React: view

---
+++ app/views/negociacoes-view.ts
@@ -0,0 +1,21 @@
+export class NegociacoesView {
+
+    template(): string {
+        return `
+
+        <table class = "table table-hover table-bordered"> 
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+                </tr>            
+            </thead>
+            <t<body>
+            
+            </tbody>
+        </table>
+        
+        `;
+    }
+}
--- dist/js/controllers/negociacao-controller.js
+++ dist/js/controllers/negociacao-controller.js
@@ -1,41 +1,30 @@
 import { Negociacao } from "../models/negociacao.js";
-import { Negociacoes } from '../models/negociacoes.js'
-
-export class NegociacaoController{
-    private inputData: HTMLInputElement;
-    private inputQuantidade: HTMLInputElement;
-    private inputValor: HTMLInputElement;
-
-    private negociacoes = new Negociacoes();
-
-    constructor(){
+import { Negociacoes } from '../models/negociacoes.js';
+export class NegociacaoController {
+    constructor() {
+        this.negociacoes = new Negociacoes();
         this.inputData = document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade');
         this.inputValor = document.querySelector('#valor');
     }
-
-    adiciona(): void{
+    adiciona() {
         const negociacao = this.criaNegociacao();
         this.negociacoes.adiciona(negociacao);
         //this.negociacoes.lista().pop(); readonly impedindo alteração da lista 
         console.log(this.negociacoes.lista());
         this.limparFormulario();
     }
-
-    criaNegociacao(): Negociacao {
+    criaNegociacao() {
         const exp = /-/g;
         const date = new Date(this.inputData.value.replace(exp, ','));
         const quantidade = parseInt(this.inputQuantidade.value);
-        const valor = parseFloat(this.inputValor.value); 
-
+        const valor = parseFloat(this.inputValor.value);
         return new Negociacao(date, quantidade, valor);
     }
-
-    limparFormulario(): void{
+    limparFormulario() {
         this.inputData.value = '';
         this.inputQuantidade.value = '';
         this.inputValor.value = '';
-
         this.inputData.focus();
     }
 }
